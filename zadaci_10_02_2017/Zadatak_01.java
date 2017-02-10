1ca7288fc9f9b07eb46e41b0031397f7c5c13ec2
zagi9 <zagoracmilan@gmail.com>
2017-02-10 12:16:43 +0100

    depo

---
+++ zadaci_10_02_2017/Zadatak_01.java
@@ -0,0 +1,62 @@
+package zadaci_10_02_2017;
+
+import java.util.Scanner;
+
+public class Zadatak_01 {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Unesite pocetni broj: ");
+		int first = sc.nextInt();
+		
+		System.out.print("Unesite krajnji broj: ");
+		int last = sc.nextInt();
+		
+		System.out.print("Koliko zelite brojeva po liniji: ");
+		int perLine = sc.nextInt();
+		
+		// U slucaju da korisnik unese 0 ili negativni broj program trazi novi unos
+		while (perLine <= 0) {
+			System.out.println("Nevazeci unos, broj linija mora biti veci od 0");
+			System.out.print("Unesite novu vrijednost: ");
+			perLine = sc.nextInt();
+		}
+		
+		printPrimes(first, last, perLine);
+		
+		sc.close();
+	}
+	
+	/*
+	 * Method ispisuje proste brojeve u rasponu argumenata first-last
+	 * Sa po, perLine, brojeva po liniji
+	 */
+	public static void printPrimes (int first, int last, int perLine) {
+		
+		int counter = 1; 
+		for (int i = first; i <= last; i++) {
+			
+			// Ako je i 0 ili 1 preskace se ispis jer ovo nisu prosti brojevi
+			if (i == 0 || i == 1) continue;
+			
+			if (isPrime(i)) {
+				if (counter % perLine == 0) System.out.printf("%5d\n", i);
+				else System.out.printf("%5d", i);
+				counter++;
+			}
+		}
+		System.out.println("\n\nU navedenom rasponu imamo ukupno " + (counter-1) +
+				" prostih brojeva");
+	}
+	
+	/*
+	 * Method provjerava da li je argument n prost broj
+	 */
+	public static boolean isPrime(int n) {
+		for (int i=2; i <= n/2; i++) 
+			if (n % i == 0) return false;
+		return true;
+	}
+
+}
